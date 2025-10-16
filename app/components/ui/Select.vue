3065a165ca1ff6c20c27b7f842aea6773011113d
Bebe Jane <bjornthief@gmail.com>
2025-10-16 17:23:02 +0200

    hej

--- app/components/ui/Select.vue
+++ app/components/ui/Select.vue
@@ -1,56 +1,66 @@
 <script setup lang="ts">
-defineProps<{
-	items: { label: string; value: string }[];
-}>();
+  defineProps<{
+    items: { label: string; value: string }[];
+  }>();
 
-const item = ref();
+  const item = ref();
 </script>
 
 <template>
-	<SelectRoot v-model="item">
-		<SelectTrigger class="SelectItem">
-			<SelectValue />
-			<SelectIcon />
-		</SelectTrigger>
+  <SelectRoot v-model="item">
+    <SelectTrigger class="SelectItem">
+      <SelectValue />
+      <SelectIcon />
+    </SelectTrigger>
 
-		<SelectPortal>
-			<SelectContent class="SelectContent">
-				<SelectScrollUpButton />
-				<SelectViewport>
-					<SelectItem v-for="({ label, value }, index) in items" :key="index" :value="value" class="SelectItem">
-						<SelectItemIndicator>
-							<Icon icon="radix-icons:check" />
-						</SelectItemIndicator>
-						<SelectItemText>
-							{{ label }}
-						</SelectItemText>
-					</SelectItem>
-					<SelectGroup>
-						<SelectLabel />
-						<SelectItem v-for="({ label, value }, index) in items" :key="index" :value="value" class="SelectItem">
-							<SelectItemIndicator>
-								<Icon icon="radix-icons:check" />
-							</SelectItemIndicator>
-							<SelectItemText>
-								{{ label }}
-							</SelectItemText>
-						</SelectItem>
-					</SelectGroup>
-					<SelectSeparator />
-				</SelectViewport>
-				<SelectScrollDownButton />
-				<SelectArrow />
-			</SelectContent>
-		</SelectPortal>
-	</SelectRoot>
+    <SelectPortal>
+      <SelectContent class="SelectContent">
+        <SelectScrollUpButton />
+        <SelectViewport>
+          <SelectItem
+            v-for="({ label, value }, index) in items"
+            :key="index"
+            :value="value"
+            class="SelectItem"
+          >
+            <SelectItemIndicator>
+              <Icon icon="radix-icons:check" />
+            </SelectItemIndicator>
+            <SelectItemText>
+              {{ label }}
+            </SelectItemText>
+          </SelectItem>
+          <SelectGroup>
+            <SelectLabel />
+            <SelectItem
+              v-for="({ label, value }, index) in items"
+              :key="index"
+              :value="value"
+              class="SelectItem"
+            >
+              <SelectItemIndicator>
+                <Icon icon="radix-icons:check" />
+              </SelectItemIndicator>
+              <SelectItemText>
+                {{ label }}
+              </SelectItemText>
+            </SelectItem>
+          </SelectGroup>
+          <SelectSeparator />
+        </SelectViewport>
+        <SelectScrollDownButton />
+        <SelectArrow />
+      </SelectContent>
+    </SelectPortal>
+  </SelectRoot>
 </template>
 
 <style lang="scss" scoped>
-.SelectContent {
-	background-color: transparent;
-}
-.SelectItem {
-	padding: 1rem;
-	background-color: var(--white);
-}
+  .SelectContent {
+    background-color: transparent;
+  }
+  .SelectItem {
+    padding: 1rem;
+    background-color: var(--white);
+  }
 </style>
