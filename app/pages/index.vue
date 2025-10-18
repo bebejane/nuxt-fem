93a21536f6a230d96540cae209f7a435c0e14057
Bebe Jane <bjornthief@gmail.com>
2025-10-18 13:40:47 +0200

    fix

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,22 +1,15 @@
 <script setup lang="ts">
   const { locale } = useI18n();
-  const { data, error, fetching } = useAllPostsQuery({
-    variables: {
-      locale: locale.value as SiteLocale,
-    },
-  });
-  /*
-  const { data, error, pending, status } = await useApiQuery<
+  const { data, error, pending } = await useApiQuery<
     AllPostsQuery,
     AllPostsQueryVariables
   >(`allPosts-${locale.value}`, AllPostsDocument, {
     variables: { locale: locale.value as SiteLocale },
   });
-  */
 </script>
 
 <template>
-  <div v-if="fetching">Loading...</div>
+  <div v-if="pending">Loading...</div>
   <pre v-else-if="error">Error: {{ JSON.stringify(error, null, 2) }}</pre>
   <div v-else>
     <ul v-for="post in data?.allPosts" :key="post.id">
